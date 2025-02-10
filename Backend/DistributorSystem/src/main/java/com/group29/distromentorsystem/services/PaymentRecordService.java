4adae02839fffc21fd8f2bbb5f8d78b38a78c7aa
Gabby0422 <calezagab@gmail.com>
2025-02-10 23:47:43 +0800

    02/10/25

--- Backend/DistributorSystem/src/main/java/com/group29/distromentorsystem/services/PaymentRecordService.java
+++ Backend/DistributorSystem/src/main/java/com/group29/distromentorsystem/services/PaymentRecordService.java
@@ -5,8 +5,6 @@ import com.group29.distromentorsystem.models.PaymentRecord;
 import com.group29.distromentorsystem.repositories.PaymentRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
