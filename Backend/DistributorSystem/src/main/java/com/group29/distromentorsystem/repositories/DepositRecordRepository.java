474de8689df2944e6d6a344878a928bf3e668b26
Gabby0422 <calezagab@gmail.com>
2025-02-11 00:13:54 +0800

    02/10/25

---
+++ Backend/DistributorSystem/src/main/java/com/group29/distromentorsystem/repositories/DepositRecordRepository.java
@@ -0,0 +1,14 @@
+package com.group29.distromentorsystem.repositories;
+
+
+import com.group29.distromentorsystem.models.DepositRecord;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface DepositRecordRepository extends MongoRepository<DepositRecord, String> {
+    List<DepositRecord> findByOrderid(String orderId);
+    List<DepositRecord> findByDealerid(String dealerId);
+}
+
